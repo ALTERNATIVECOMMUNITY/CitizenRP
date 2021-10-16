594766e2646646687bfb18363023cd382c3be97b
Forley <forleyfr@gmail.com>
2021-10-16 16:33:38 +0200

    Sheriff

--- resources/[script]/[esx]/[job]/[leo]/esx_sheriff/client/main.lua
+++ resources/[script]/[esx]/[job]/[leo]/esx_sheriff/client/main.lua
@@ -81,7 +81,7 @@ function OpenCloakroomMenu()
 		table.insert(elements, {label = '---TÊTE---' , uniform = ''})
 	--	table.insert(elements, {label = 'Bonet' , uniform = 'bonet'})
 
-		table.insert(elements, {label = 'Campagne' , uniform = 'chapo'})
+		table.insert(elements, {label = 'Campagne', uniform = 'chapo'})
 		table.insert(elements, {label = 'Casquette Pat' , uniform = 'casquette'})
 		table.insert(elements, {label = 'Casquette Narco' , uniform = 'casquette2'})
 		table.insert(elements, {label = 'Casquette K9' , uniform = 'cask9'})
@@ -92,7 +92,7 @@ function OpenCloakroomMenu()
 		table.insert(elements, {label = 'Emeute' , uniform = 'emeute'})
 		table.insert(elements, {label = 'Retiré Casque', uniform = 'offcasquette'})
 
- 
+
 		table.insert(elements, {label = '________TENUE________' , uniform = ''})
 
 
