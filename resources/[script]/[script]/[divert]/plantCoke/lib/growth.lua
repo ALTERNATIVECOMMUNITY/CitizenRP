f9c24e0b0b7884965478144a3926206181e1608f
Cyril Beeckman <cyril.beeckman@gmail.com>
2021-10-21 15:12:04 +0200

    Add new drugs plantation

---
+++ resources/[script]/[script]/[divert]/plantCoke/lib/growth.lua
@@ -0,0 +1,93 @@
+--[[ PLANT GROWTH STAGES
+
+Each plant starts out at stage 1 and starting consumes
+one Config.Items.Seed immediately, and the first timer starts running.
+Interacting with the plant at this time destroys it.
+
+When the time is up, it progresses to the next stage, which is
+a mode='tend' stage. Interaction at this stage consumes one
+Config.Items.Tend, or is free if the Tend Object is not specified.
+Interaction is possible at any time. Failure to interact before the
+time runs out causes the plant to die.
+
+This continuses for all grow/tend cycles until a mode='yield' cycle
+is arrived at. Interacting with the plant at this time will yield
+between Config.Yield[1] and Config.Yield[2] of Config.Items.Product
+and between Config.YieldSeed[1] and Config.YieldSeed[2] seeds.
+By default this means between 5 and 10 weed_pooch, and between 0
+and 1 weed_seed
+
+--]]
+
+local Colors = {
+    Growing = {0, 255, 128, 128},
+    Tend = {255, 255, 128, 128},
+    Harvest = {255, 128, 128, 128},
+}
+
+Growth = {
+    { -- 1
+        label = 'growth_seedling',
+        model = `vw_prop_casino_art_plant_06a`,
+        offset = vector3(0,0,-1.9),
+        time = 30,
+        marker = {
+            offset = vector3(0,0,0),
+            color = Colors.Growing,
+        },
+    },
+    { -- 2
+        label = 'growth_tend',
+        model = `vw_prop_casino_art_plant_06a`,
+        offset = vector3(0,0,-1.6),
+        interact = true,
+        time = 240,
+        marker = {
+            offset = vector3(0,0,0),
+            color = Colors.Tend,
+        },
+    },
+    { -- 3
+        label = 'growth_growing',
+        model = `vw_prop_casino_art_plant_06a`,
+        offset = vector3(0,0,-1.35),
+        time = 30,
+        marker = {
+            offset = vector3(0,0,0),
+            color = Colors.Growing,
+        },
+    },
+    { -- 4
+        label = 'growth_tend',
+        model = `vw_prop_casino_art_plant_06a`,
+        offset = vector3(0,0,-1.1),
+        interact = true,
+        time = 240,
+        marker = {
+            offset = vector3(0,0,0),
+            color = Colors.Tend,
+        },
+    },
+    { -- 5
+        label = 'growth_growing',
+        model = `vw_prop_casino_art_plant_06a`,
+        offset = vector3(0,0,-0.9),
+        time = 30,
+        marker = {
+            offset = vector3(0,0,0),
+            color = Colors.Growing,
+        },
+    },
+    { -- 6
+        label = 'growth_yield',
+        model = `vw_prop_casino_art_plant_06a`,
+        offset = vector3(0,0,-0.65),
+        interact = true,
+        yield = true,
+        time = 480,
+        marker = {
+            offset = vector3(0,0,0),
+            color = Colors.Harvest,
+        },
+    },
+}
---
+++ resources/[script]/[script]/[divert]/plantCoke/locales/fr-FR.lua
@@ -0,0 +1,30 @@
+Locales['fr-FR'] = {
+    planting_text = 'Plantation',
+    planting_ok = 'Bien plantée !',
+    planting_in_vehicle = 'Impossible de planter dans un vehicule !',
+    planting_too_steep = 'Trop raide ici !',
+    planting_too_far = 'Un peu plus proche...',
+    planting_not_suitable_soil = 'Ca ne va pas pousser ici...',
+    planting_too_close = 'La plante a besoin de plus d\'espace !',
+    planting_obstructed = 'Cherche un endroit avec plus de lumière',
+    planting_no_seed = 'T\'as plus assez de graines',
+    planting_failed = '~r~Ta pelle s\'est cassée',
+    planting_too_fast = 'Chillax, bro! Attend un petit peu.',
+
+    status_active = '~INPUT_PICKUP~ ~a~~n~~INPUT_COVER~ Détruire',
+    status_passive = '~a~~n~~INPUT_COVER~ Détruire',
+
+    growth_seedling = 'Fraîchement plantée.~n~Elle aura bientôt besoin de vous!',
+    growth_tend = 'Mettre de l\'engrais',
+    growth_growing = 'Elle pousse à son rythme.',
+    growth_yield = 'Récolter',
+
+    command_invalid = 'Invalid directive: %s',
+    command_empty = 'Version %s',
+
+    interact_text = 'Interraction',
+    interact_tended = 'Votre arrosage fait effet',
+    interact_destroyed = 'Plante détruite',
+    interact_full = 'Pas assez de place sur toi',
+    interact_missing_item = 'Tu n\'as pas d\'eau purifiée sur toi!',
+}
