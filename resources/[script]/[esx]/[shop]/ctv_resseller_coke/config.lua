2389ceb2df8d9609c24027a4e7b1a1536647a0f0
Forley <forleyfr@gmail.com>
2021-10-26 19:57:16 +0200

    Ajout points ressellers

---
+++ resources/[script]/[esx]/[shop]/ctv_resseller_coke/config.lua
@@ -0,0 +1,23 @@
+Config = {}
+
+-- Change menu alignment
+Config.MenuAlign = "top-left"
+
+-- Change items that can be sold at the pawnshop, the number behind = is the price
+Config.PawnshopItems = {
+	coke = 1,
+}
+
+Config.GiveBlack = false -- give black money? if disabled it'll give regular cash.
+
+Config.PawnshopLocation =  {x = 991.31524658203, y = -2195.0480957031, z = 31.587823867798}
+
+-- Pawnshop blip
+Config.PawnshopBlipText = ""
+Config.PawnshopBlipColor = 0
+Config.PawnshopBlipSprite = 0
+
+-- Opening hours
+Config.EnableOpeningHours = true -- Enable opening hours? If disabled you can always open the pawnshop.
+Config.OpenHour = 21 -- From what hour should the pawnshop be open?
+Config.CloseHour = 6 -- From what hour should the pawnshop be closed?
---
+++ resources/[script]/[esx]/[shop]/ctv_resseller_coke/fxmanifest.lua
@@ -0,0 +1,16 @@
+name "r3_pawnshop"
+author "r3ps4J"
+contact "discord.gg/bEWmBbg"
+
+fx_version "adamant"
+game "gta5"
+
+description "A simple pawnshop for ESX"
+
+
+
+client_script "scripts/cl_*.lua"
+server_script "scripts/sv_*.lua"
+
+client_script "config.lua"
+server_script "config.lua"
