1c2285b8ead2531eb41df1312934af555fc6a620
Cyril Beeckman <cyril.beeckman@gmail.com>
2021-10-28 16:25:48 +0200

    Add rocade

--- resources/[fivem]/[WL]/Badger_Discord_API/client.lua
+++ resources/[fivem]/[WL]/Badger_Discord_API/client.lua
@@ -1,8 +1,8 @@
 triggered = false;
 AddEventHandler("playerSpawned", function()
-    if not triggered then 
+    if not triggered then
         triggered = true;
-        Citizen.Wait((1000 * 20)); -- Wait 20 seconds
+        Citizen.Wait((1000)); -- Wait 20 seconds
         TriggerServerEvent('Badger_Discord_API:PlayerLoaded');
     end
 end)
--- resources/[fivem]/[WL]/Badger_Discord_API/config.lua
+++ resources/[fivem]/[WL]/Badger_Discord_API/config.lua
@@ -1,27 +1,25 @@
 Config = {
-	Guild_ID = '892427445272211537',
-	Bot_Token = 'NzE3OTExNTAzMTUxNTYyODEz.XthNMA.rkQwSkK9KMQtR7UW5_mml5Dqy58',
-	RoleList = {
-		 ['Citoyens'] = 892431113388195920,
-		 ['vip'] = 892444998723850320,
-		  ['Fonda'] = 892430820411834398,
-			 ['Gestions'] = 892430911604416554,
-			  ['Dev'] = 892430977383686224,
-			 ['MODERATION'] = 892431189175046184,
-			  ['streamer'] = 892445066424111134,
-			  ['Testeur'] = 895276272903667742,
+    Guild_ID = '892427445272211537',
+    Bot_Token = 'NzE3OTExNTAzMTUxNTYyODEz.XthNMA.rkQwSkK9KMQtR7UW5_mml5Dqy58',
+    RoleList = {
+        ['Citoyens'] = 892431113388195920,
+        ['vip'] = 892444998723850320,
+        ['Fonda'] = 892430820411834398,
+        ['Gestions'] = 892430911604416554,
+        ['Dev'] = 892430977383686224,
+        ['MODERATION'] = 892431189175046184,
+        ['streamer'] = 892445066424111134,
+        ['Testeur'] = 895276272903667742
 
-
-
-	 },
+    }
 }
 
 Config.Splash = {
-	Header_IMG = 'https://cdn.discordapp.com/attachments/477543918599143424/894975311853269012/Rocade.png',
-	Enabled = true,
-	Wait = 10, -- How many seconds should splash page be shown for? (Max is 12)
-	Heading1 = "Bienvenue sur Citizen-Vice",
-	Heading2 = "Soyez sur de rejoindre le discord !",
-	Discord_Link = 'https://discord.gg/S4VBFx9v5g',
-	Website_Link = 'https://citizen-vice.fr',
+    Header_IMG = 'https://cdn.discordapp.com/attachments/477543918599143424/894975311853269012/Rocade.png',
+    Enabled = false,
+    Wait = 10, -- How many seconds should splash page be shown for? (Max is 12)
+    Heading1 = "Bienvenue sur Citizen-Vice",
+    Heading2 = "Soyez sur de rejoindre le discord !",
+    Discord_Link = 'https://discord.gg/S4VBFx9v5g',
+    Website_Link = 'https://citizen-vice.fr'
 }
