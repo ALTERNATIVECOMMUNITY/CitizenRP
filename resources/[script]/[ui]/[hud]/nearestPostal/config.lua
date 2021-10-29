03f565e72a3db2b65cbdf6206509b82a705f9f77
Cyril Beeckman <cyril.beeckman@gmail.com>
2021-10-29 21:13:05 +0200

    Clean base

--- resources/[script]/[ui]/[hud]/nearestPostal/config.lua
+++ resources/[script]/[ui]/[hud]/nearestPostal/config.lua
@@ -1,21 +1,21 @@
 config = {
-	versionCheck = true, -- enables version checking (if this is enabled and there is no new version it won't display a message anyways)
-	text = {
-		format = '~y~Code Postal~w~: %s',
-		-- ScriptHook PLD Position
-		--posX = 0.225,
-		--posY = 0.963,
-		-- vMenu PLD Position
-		posX = 0.145,
-		posY = 0.968
-	},
-	blip = {
-		blipText = 'Itinéraire Postal %s',
-		sprite = 8,
-		color = 3, -- default 3 (light blue)
-		distToDelete = 100.0, -- in meters
-		deleteText = 'Route deleted',
-		drawRouteText = 'Drawing a route to %s',
-		notExistText = "That postal doesn't exist"
-	}
+    versionCheck = true, -- enables version checking (if this is enabled and there is no new version it won't display a message anyways)
+    text = {
+        format = '~y~Code Postal~w~: %s',
+        -- ScriptHook PLD Position
+        -- posX = 0.225,
+        -- posY = 0.963,
+        -- vMenu PLD Position
+        posX = 0.165,
+        posY = 0.968
+    },
+    blip = {
+        blipText = 'Itinéraire Postal %s',
+        sprite = 8,
+        color = 3, -- default 3 (light blue)
+        distToDelete = 100.0, -- in meters
+        deleteText = 'Route deleted',
+        drawRouteText = 'Drawing a route to %s',
+        notExistText = "That postal doesn't exist"
+    }
 }
